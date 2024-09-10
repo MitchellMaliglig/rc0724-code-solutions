972afbf1ca7c636e5e9fe3bf3c30e6c13a721b55
MitchellMaliglig <mitchell.maliglig@gmail.com>
2024-09-10 19:29:10 +0000

    completed express-get

---
+++ express-get/server.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+import pg from 'pg';
+import { ClientError, errorMiddleware } from './lib/index.js';
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/pagila',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
+
+app.get('/api/films', async (req, res, next) => {
+  try {
+    const sql = `
+      SELECT "title", "filmId", "replacementCost"
+      FROM "films"
+      ORDER BY "replacementCost" DESC
+      LIMIT 2;
+      `;
+    const result = await db.query(sql);
+    const films = result.rows;
+    res.json(films);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/films/:filmId', async (req, res, next) => {
+  try {
+    const { filmId } = req.params;
+    if (!filmId) {
+      throw new ClientError(400, 'filmId undefined');
+    } else if (Number.isInteger(+filmId)) {
+      throw new ClientError(400, 'non-integer filmId');
+    }
+    const sql = `
+      SELECT "title", "filmId"
+      FROM "films"
+      WHERE "filmId" = $1;
+      `;
+    const params = [filmId];
+    const result = await db.query(sql, params);
+    const film = result.rows[0];
+    if (!film) {
+      throw new ClientError(404, 'film not found');
+    }
+    res.json(film);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorMiddleware);
+
+app.listen(8080, () => {
+  console.log('listening on port 8080');
+});
